709f452f47f8d07f2384a85cd498e5f06ec0acf1
Steven Richard <s.richard.dev@gmail.com>
2019-02-17 22:59:04 +0100

    adjusted import script

--- import_assets.sh
+++ import_assets.sh
@@ -1,20 +1,46 @@
 #!/bin/bash
 
 source_path="/Users/steven/SRC/pokemontool-ios/PokemonGoTool/PokemonGoTool/Assets.xcassets"
-target_path="/Users/steven/SRC/pokemontool-android/app/src/main/assets"
+assets_path="/Users/steven/SRC/pokemontool-android/app/src/main/assets"
+drawables_path="/Users/steven/SRC/pokemontool-android/app/src/main/res/drawable"
+path_svg2vector_tool="/Users/steven/SRC/tools/Svg2VectorAndroid-1.0.jar"
 
-### for multiple names to move
-#find "$source_path" -type f \( -name "*.png" -o -name "*.pdf" \) -exec mv -i {} "$target_path"  \;
 
 ### copy and convert pdf to svg
-find "$source_path" -type f  -name "*.pdf" -exec cp -i {} "$target_path"  \;
+find "$source_path" -type f  -name "*.pdf" -exec cp -i {} "$assets_path"  \;
 
-for file in "$target_path"/*.pdf; do
+for file in "$assets_path"/*.pdf; do
     basename=$(basename "$file" .pdf)
-    pdf2svg "$target_path"/"$basename".pdf "$target_path"/"$basename".svg
+    pdf2svg "$assets_path"/"$basename".pdf "$assets_path"/"$basename".svg
 done
 
-rm -f "$target_path"/*.pdf
+### clean up
+rm -f "$assets_path"/*.pdf
 
+### remove all old "_svg.xml" files in drawables
+for file in "$drawables_path"/*.xml; do
+    if [[ $file == *"_svg.xml" ]]; then
+        rm -f "$file"
+    fi
+done
+
+### convert all svg in xml and mv to drawables directory
+java -jar "$path_svg2vector_tool" "$assets_path"
+processed_svg_path="$assets_path"/ProcessedSVG
+
+# rename all .xml to lovercase
+for f in "$processed_svg_path"/*.xml; do
+    mv "$f" "$f.tmp"; mv "$f.tmp" "`echo $f | tr "[:upper:]" "[:lower:]"`";
+done
+
+find "$assets_path" -type f -name "*.xml" -exec mv {} "$drawables_path"  \;
+
+### clean up
+rm -rf "$processed_svg_path"
+
+
+### TODOs
 ### copy appicon png to androids directory structur
 
+### for multiple names to move
+#find "$source_path" -type f \( -name "*.png" -o -name "*.pdf" \) -exec mv -i {} "$assets_path"  \;
