90c6bda3078db26ddd33e9963c37339a95d8d15b
Steven Richard <s.richard.dev@gmail.com>
2019-06-30 12:24:06 +0200

    changed app icon and name

--- app/src/main/java/io/stanc/pogotool/screen/PolicyFragment.kt
+++ app/src/main/java/io/stanc/pogotool/screen/PolicyFragment.kt
@@ -16,12 +16,16 @@ class PolicyFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootLayout = inflater.inflate(R.layout.fragment_policy, container, false)
 
-        rootLayout.findViewById<Button>(R.id.policy_license)?.setOnClickListener {
-            startActivity(Intent(context, OssLicensesMenuActivity::class.java))
-        }
+        // TODO:...
+//        rootLayout.findViewById<Button>(R.id.policy_terms_conditions)?.setOnClickListener {
+//        }
 
         rootLayout.findViewById<Button>(R.id.policy_privacy)?.setOnClickListener {
             startActivity(Intent(context, PrivacyPolicyActivity::class.java))
+        }
+
+        rootLayout.findViewById<Button>(R.id.policy_license)?.setOnClickListener {
+            startActivity(Intent(context, OssLicensesMenuActivity::class.java))
         }
 
         return rootLayout
